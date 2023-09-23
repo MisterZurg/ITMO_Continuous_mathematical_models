3988e43e8ae79460694346fd3506aef5ec2786a4
MisterZurg <misterzurg@gmail.com>
2023-09-23 16:09:44 +0300

    Refactored EulerMethod

--- Assignment-3-Numerical-solutions.go
+++ Assignment-3-Numerical-solutions.go
@@ -16,12 +16,18 @@ var INTERVAL = [2]float64{0, 0.5}
 
 const STEP float64 = 0.1
 
+//var n = int(INTERVAL[1] / STEP)
+//var ys = make([]float64, n)
+
 // EulerMethod is implementation from slide 13
-func EulerMethod(xi, yi float64) float64 {
-	if xi == INTERVAL[0] {
-		return Equation(xi)
+func EulerMethod(xs, ys, yds []float64) []float64 {
+	n := len(xs)
+	yem := make([]float64, n)
+	yem[0] = ys[0]
+	for i := 1; i < n; i++ {
+		yem[i] = yem[i-1] + STEP*yds[i-1]
 	}
-	return 0 // EulerMethod(yi-1) + STEP
+	return yem
 }
 
 func EulerCauchyMethod(interval [2]float64, h float64) {
@@ -46,7 +52,7 @@ func Equation(x float64) float64 {
 }
 
 // FirstOrderDifferentialEquation() computes y' = (y + x)^2
-func FirstOrderDifferentialEquation(x, y float64) float64 {
+func FirstOrderDerivative(x, y float64) float64 {
 	if y == 0 {
 		return 0
 	}
@@ -55,12 +61,25 @@ func FirstOrderDifferentialEquation(x, y float64) float64 {
 }
 
 func main() {
-	fmt.Printf("x \t| y_true\n")
-	for x := INTERVAL[0]; x < INTERVAL[1]; x += STEP {
-		fmt.Println(Answer(x))
+	xs, ys, yds := getValues()
+	yme := EulerMethod(xs, ys, yds)
+	//fmt.Printf("x \t| y_true\n")
+	for _, val := range yme {
+		fmt.Println(val)
 	}
 }
 
-func Answer(x float64) string {
-	return fmt.Sprintf("%.2f\t| %.9f", x, Equation(x))
+func getValues() ([]float64, []float64, []float64) {
+	var xs, ys, yds []float64
+
+	for x := INTERVAL[0]; x < INTERVAL[1]; x += STEP {
+		xs = append(xs, x)
+		ys = append(ys, Equation(x))
+		yds = append(yds, FirstOrderDerivative(x, ys[len(ys)-1]))
+	}
+	return xs, ys, yds
+}
+
+func Answer(x, yTrue float64) string {
+	return fmt.Sprintf("%.2f\t| %.9f", x, yTrue)
 }
